059d5bd9050275c43cd3d7e552f5e0c77e57af3c
km199 <kai.mcgrath@me.com>
2024-07-07 20:01:45 -0700

    Integrated Coin Market Cap

--- src/main/java/org/example/MiniTicker.java
+++ src/main/java/org/example/MiniTicker.java
@@ -14,22 +14,13 @@ public final class MiniTicker implements Runnable{
     private static final Logger logger
             = LoggerFactory.getLogger(MiniTicker.class);
     private String ticker;
-    private double lastAlert;
-    private double percentAlert;
-    private double price;
-    public long lastTime;
-    private HelloBot bot;
-    private String chatID;
     private WebSocketStreamClient client;
-    public MiniTicker(String ticker, double lastAlert, double percentAlert, HelloBot bot, String chatID) {
+    private Crypto token;
+    private long lastTime;
+    public MiniTicker(String ticker, Crypto token) {
         this.ticker = ticker;
-        this.lastAlert = lastAlert;
-        this.percentAlert = percentAlert;
-        this.bot = bot;
-        this.chatID = chatID;
-        this.lastTime = 0;
+        this.token = token;
         client = new WebSocketStreamClientImpl();
-
     }
 
     public static HashMap<String, String> read(String str) {
@@ -42,38 +33,14 @@ public final class MiniTicker implements Runnable{
     public void run() {
         int connection = client.miniTickerStream(ticker, ((event) -> {
             HashMap<String, String> emap = read(event);
-            price = Double.parseDouble(emap.get("c"));
-            lastTime = System.currentTimeMillis() / 1000L;
-            double diff = price - lastAlert;
-            double percent = (diff/lastAlert)*100;
-            if (Math.abs(percent) > percentAlert) {
-                //Send Message
-                StringBuilder msg = new StringBuilder();
-                double oldLastAlert = lastAlert;
-                msg.append(ticker.toUpperCase());
-                if (diff > 0) {
-                    msg.append(" up ");
-                    lastAlert = lastAlert*(1+percentAlert/100);
-                } else {
-                    msg.append(" down ");
-                    lastAlert = lastAlert*(1-percentAlert/100);
-                }
-                msg.append(percentAlert);
-                msg.append("% from ");
-                msg.append(RoundDouble.round(oldLastAlert, 2));
-                msg.append(" to ");
-                msg.append(RoundDouble.round(lastAlert, 2));
-                TelegramApiException error = bot.send(msg.toString(), chatID);
-                if (error != null) {
-                    logger.error("Message Send Failure");
-                    logger.error(error.getMessage());
-                }
-            }
+            double price = Double.parseDouble(emap.get("c"));
             logger.debug(String.valueOf(price));
+            token.update(price);
+            lastTime = System.currentTimeMillis() / 1000L;
         }));
         while (true) {
             try {
-                TimeUnit.SECONDS.sleep(5);
+                TimeUnit.SECONDS.sleep(10);
             } catch (InterruptedException e) {
                 ;
             }
@@ -84,10 +51,10 @@ public final class MiniTicker implements Runnable{
                 if (stale > Settings.TICKER_RESTART_TIME_SECONDS) {
                     lastTime = 0;
                     client.closeConnection(connection);
-                    logger.info("Possibly stale connection, restarting ticker in 15 seconds");
+                    logger.info("Possibly stale connection, restarting ticker in 30 seconds");
                     //We can't reconnect immediately
                     try {
-                        TimeUnit.SECONDS.sleep(15);
+                        TimeUnit.SECONDS.sleep(30);
                     } catch (InterruptedException e) {
                         ;
                     }
--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,10 +1,13 @@
 import org.example.Cmc;
 import org.example.Crypto;
 
+import static org.example.Settings.ALERT_PERCENT_SOL;
+import static org.example.Settings.ALERT_PERCENT_TRUNK;
+
 public class test {
     public static void main(String[] args) {
-        Crypto solana = new Crypto("Solana");
-        Crypto trunk = new Crypto("Trunk");
+        Crypto solana = new Crypto("Solana", ALERT_PERCENT_SOL);
+        Crypto trunk = new Crypto("Trunk", ALERT_PERCENT_TRUNK);
         solana.load();
         trunk.load();
         Cmc api = new Cmc(solana, trunk);
